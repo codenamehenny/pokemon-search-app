1f63ebf5ac2d18379ef2cb081726e5bb2bdb4b98
codenamehenny <genesis09m@hotmail.com>
2025-06-23 15:20:43 -0400

    script file added, function to fetch data from API and translating it over

---
+++ script.js
@@ -0,0 +1,45 @@
+// Search function for Pokemon
+async function searchPokemon() {
+    const input = document.getElementById("pokemonInput").value.trim().toLowerCase();
+    const container = document.getElementById("pokemonContainer");
+    const errorDiv = document.getElementById("errorMessage")
+
+// this section clears previous errors or results
+errorDiv.textcontent = "";
+container.innerhtml = "";
+
+if (!input) {
+    errorDiv.textContent = "Please enter a Pokemon name or ID";
+    return;
+}
+
+try {
+    // using the input to fetch data from the PokeAPI 
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/${input}');
+    
+    if (!response.ok) {
+        throw new Error("Whoops! Pokemon not found");
+    }
+
+    const data = await response.json();
+
+    // Create and populate html elements with Pokemon data
+    const name = document.createElement("h2");
+    name.textContent = 'Name: ${data.name}';
+
+    const image = document.createElement("img");
+    image.src = data.spites.front_default;
+    image.alt = data.name;
+
+    const type = document.createElement("p");
+    type.textContent = 'Type: ${data.types.map(t => t.type.name).join(', ')}';
+
+    // Appending elements to container 
+    container.appendChild(name);
+    container.appendChild(image);
+    container.appendChild(type);
+ } catch (error) {
+    errorDiv.textContent = error.message;
+ }
+
+}
